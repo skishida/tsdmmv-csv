7a5de945b7c337904af9ea1f4160d7d7162bd98e
skishida <shoki0417@gmail.com>
2021-05-05 21:08:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Ts Digital Multi Meter Viewer Logfile Converter
+
+convert log csv file to user-friendly csv and json file
+
+### Fromat 
+
+* original csv file
+    ```csv
+    [TsDigitalMultiMeterViewer Ver.11.4.0 : [OWON B35/B35T (Bluetooth2.0)] : Date 2021/05/05 16:11:45],test Fig,test Meter Title
+    Elapsed time | Main Value               | Mode  | Date Time
+      0:00'00"12 , DC    ,  356.80 ,     mV ,       , 2021/05/05 15:46:57,
+      0:00'00"64 , DC    ,  355.90 ,     mV ,       , 2021/05/05 15:46:57,
+    ```
+
+* output csv file for main data
+    ```csv
+    Elapsed time,Mode,Main Value,Unit,Mode,Date Time, Elapsed sec
+    0:00:00.12,DC,356.80,mV,,2021/05/05 15:46:57,0.12
+    0:00:00.64,DC,355.90,mV,,2021/05/05 15:46:57,0.64
+    ```
+  * remove blank
+  * change date format from `hh:mm'ss"uuu` to `hh:mm:ss.uuu`
+  * add `Elapsed sec` field
+
+
+* output json file for meta data
+    ```json
+    {
+      "Version": "TsDigitalMultiMeterViewer Ver.11.4.0",
+      "Meter": "OWON B35/B35T (Bluetooth2.0)",
+      "Save Date": "2021/05/05 16:11:45",
+      "Fig Title": "test Fig",
+      "Meter Title": "test Meter Title"
+    }
+    ```
+
+### Usage
+
+* [Open this page in github pages]() and just drag&drop `.csv` file into browser
+  * or clone this repository and open `index.html` locally
+
+### Reference
+
+* [Ts Digital Multi Meter Viewer](http://www.ts-software-jp.net/products/tsdmmview.html)
---
+++ app.js
@@ -0,0 +1,132 @@
+const inputElement = document.getElementById("input");
+inputElement.addEventListener("change", inputChangedHandler, false);
+function inputChangedHandler() {
+    processFiles(this.files);
+    document.getElementById('input').value = ''
+}
+
+if (window.File) {
+    var dropArea = document.getElementById('dropArea');
+
+    dropArea.addEventListener('dragover', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+    }, false);
+
+    dropArea.addEventListener('drop', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        var files = e.dataTransfer.files;
+        processFiles(files);
+    }, false);
+}
+
+function processFiles(files) {
+    for (var i = 0; i < files.length; i++) {
+        var f = files[i];
+        console.log(f);
+
+        // check file type
+        if (f.name.endsWith(".csv") && f.type.match(/application|csv/)) {
+        } else {
+            alert('wrong format file selected!');
+            return;
+        };
+
+        var reader = new FileReader();
+
+        reader.onerror =
+            function () {
+                alert('error on file reading!')
+            };
+        reader.addEventListener('load', { file: f, handleEvent: areaLoadedHandler });
+
+        reader.readAsText(f);
+    }
+}
+
+function areaLoadedHandler(e) {
+    const res = e.target.result;
+    const filename = this.file.name;
+    const data = res.split('\n');
+    process(data, filename);
+}
+
+function process(data, filename) {
+    namebase = filename.substring(0, filename.lastIndexOf("."));
+
+    // check csv contains "TsDigitalMultiMeterViewer"
+    if (data[0].indexOf("TsDigitalMultiMeterViewer") == -1) {
+        alert('wrong csv format!')
+        return;
+    }
+
+    // read first line
+    var meta =
+    {
+        "Version": "",
+        "Meter": "",
+        "Save Date": "",
+        "Fig Title": "",
+        "Meter Title": ""
+    };
+    metaTitle = data[0].split(",");
+    metaData = metaTitle[0].split(":");
+    meta["Version"] = metaData[0].trim().replace("[", "");
+    meta["Meter"] = metaData[1].trim().replace("[", "").replace("]", "");
+    date = metaData.slice(2, metaData.length).join(":").replace("]", "").replace("Date", "");
+    meta["Save Date"] = date.trim();
+    meta["Fig Title"] = metaTitle[1].trim()
+    meta["Meter Title"] = metaTitle[2].trim()
+
+    // convert & export json
+    const metajson = JSON.stringify(meta, undefined, 2);
+    donwloadJson(metajson, namebase + "_meta");
+
+    // skip second line
+    var csv = "Elapsed time,Mode,Main Value,Unit,Mode,Date Time, Elapsed sec\n";
+
+    // read following line
+    var csvData = [];
+    for (var i = 0; i < data.length - 2; i++) {
+        var line = data[i + 2].split(",");
+        csvData[i] = line;
+
+        // remove space
+        for (var j = 0; j < csvData[i].length; j++) {
+            csvData[i][j] = csvData[i][j].trim();
+        }
+        // change date format
+        csvData[i][0] = csvData[i][0].replace("'", ":").replace("\"", ".")
+        const d = "1970-01-01 " + csvData[i][0] + " +0000";
+        const date = new Date(d);
+        csvData[i][line.length - 1] = date.getTime() / 1000.0;
+
+        csv += csvData[i].join(",") + "\n";
+    }
+
+    // export csv
+    console.log("total size:" + csv.length);
+    downloadCsv(csv, namebase + "_csv");
+}
+
+function donwloadJson(json, name) {
+    var data = "data:text/json;charset=utf-8," + encodeURIComponent(json);
+    fname = name + ".json";
+    openLink(data, fname);
+}
+
+function downloadCsv(csv, name) {
+    var data = "data:text/csv;charset=utf-8," + encodeURIComponent(csv);
+    fname = name + ".csv";
+    openLink(data, fname);
+}
+
+function openLink(data, fname) {
+    var anchor = document.createElement('a');
+    anchor.setAttribute("href", data);
+    anchor.setAttribute("download", fname);
+    document.body.appendChild(anchor);
+    anchor.click();
+    anchor.remove();
+}
